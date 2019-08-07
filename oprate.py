de04db606cce782a7fe80b49aa1a00e6c1330bd7
peterfei <peterfeispace@gmail.com>
2019-08-07 14:24:39 +0800

    优化配置

--- oprate.py
+++ oprate.py
@@ -9,20 +9,22 @@ def bash_shell(bash_command):
 
 if __name__ == '__main__':
   print('****正在注备环境****')
-  bash_shell('rm -fr node_modules')
-  # print(os.system('pwd'))
-  
   print("***执行yarn安装相关package***")
-  # bash_shell('yarn')
+  bash_shell('yarn')
+  print("***拷贝补丁文件***")
+  bash_shell('cp ./link.js ./node_modules/react-native/local-cli/link/link.js')
   print("***拷贝Android Setting***")
-  path = os.popen('pwd').read()
-  print("当前路径:",path)
-  settingPath = path+"/android/settings.gradle"
-  print("settingPath:",settingPath)
-  command= "rm  %s" % {settingPath}
+  # path = os.popen('pwd').read()
+  # print("当前路径:",path)
+  # settingPath = path+"/android/settings.gradle"
+  # print("settingPath:",settingPath)
+  # command= "rm  %s" % {settingPath}
   # os.system(command) 
   # bash_shell('cp ./android/settings.gradle.bak ./android/settings.gradle')
   # bash_shell('cp ./android/app/src/main/java/com/orghexpert/MainApplication.java.bak ./android/app/src/main/java/com/orghexpert/MainApplication.java')
-  # bash_shell('react-native link')
+  bash_shell('react-native link')
   print("***Unity相关资源拷贝***")
-
+  print("***拉取相关代码***")
+  bash_shell('git pull origin dev')
+  print("******打包******")
+  bash_shell("react-native run-android")
