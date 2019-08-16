8c8afa383fafba210430148790beeaede8d0fbc9
peterfei <peterfeispace@gmail.com>
2019-08-16 17:07:46 +0800

    微信支付

--- View/scene/Vip/BuyVip.js
+++ View/scene/Vip/BuyVip.js
@@ -10,6 +10,9 @@ import { storage } from "../../common/storage";
 import memberBackground from '../../img/vip/memberBackground.png'
 import member_center_details from '../../img/vip/member_center_details.png'
 import api from "../../api";
+import {Wxpay} from "../../common";
+import Toast, { DURATION } from "react-native-easy-toast";
+
 
 export default class BuyVip extends BaseComponent {
     constructor(props) {
@@ -23,6 +26,10 @@ export default class BuyVip extends BaseComponent {
             memberCenterDetailsHeight: 10,
             isUse: false, // 是否是会员
             PayData:'',
+            priceId: this.props.comboList.length > 0 ? this.props.comboList[0].price_id : "",
+            comboId: this.props.comboList.length > 0 ? this.props.comboList[0].combo_id : "",
+            productID: this.props.comboList.length > 0 ? this.props.comboList[0].product_id : "",
+            OrderNo: "",
         }
     }
 
@@ -54,7 +61,7 @@ export default class BuyVip extends BaseComponent {
                 })
             })
     }
-    async newAddOrder(priceId, comboId) {
+    async getOrderId(priceId, comboId) {
         let tokens = await storage.get("userTokens");
         let body = {
             "priceId": priceId,
@@ -72,13 +79,168 @@ export default class BuyVip extends BaseComponent {
             body: JSON.stringify(body)
         }).then(resp => resp.json())
             .then(result => {
-                this.setState({
-                    PayData: result
-                })
-                alert(JSON.stringify(result))
+                return new Promise((resolve, reject) => {
+                    // alert(`result is ${JSON.stringify(result)}`)
+                    // debugger;
+                    if (result && result.order) {
+                        // alert(`=====\n生成的订单号为:${JSON.stringify(result)}`);
+                        //storage.save('containsStructCombo', id, result.page);
+
+                        resolve && resolve(result.order);
+                    } else {
+                        reject && reject(new Error("data parse error"));
+                    }
+                });
             })
+            this.setState({
+                OrderNo: responseData.ordNo
+            });
     }
 
+    async payment(priceId,comboId) {
+        try {
+            this.Loading.show("正在支付...");
+            let isSupported = await Wxpay.isSupported();
+            // alert(`isSupported is ${isSupported}`)
+            console.log("---------------if support wechat--------" + isSupported);
+
+            // 生成OrderId
+            await this.getOrderId(priceId,comboId);
+            console.log("****getOrderId****");
+            // 取微信支付配置
+            const data = await this.tenPay();
+            // debugger
+            console.log(
+                "-------**data from wechat pay response is **------" +
+                JSON.stringify(data)
+            );
+            if (!isSupported) {
+                // 判断是否支持微信支付
+                this.Loading.close();
+                this.refs.toast.show("找不到微信应用，请安装最新版微信");
+                return;
+            }
+            console.log("++++++++++++++++++++++++++++++");
+            /*
+                             *{"Ry_result":"88888","Data":[{"appid":"wx6c7109324ebb9409","partnerid":"1480276662","prepayid":"wx0415254561686926b8cfdcec1274015844","package":"Sign=WXPay","noncestr":"E7FKHAE3M2NO7LEVK29O","timestamp":"1525418746","sign":"6299ADC68A23F4DC0F364245FC2997A3"}]}
+                             */
+            let ret = await Wxpay.pay(data);
+            console.log("==========================" + JSON.stringify(ret));
+            // debugger
+            // alert("-----ret-----" + JSON.stringify(ret) );
+            // alert(ret.errCode)
+            if (ret.errCode === 0) {
+                this.refs.toast.show("支付成功");
+
+                this.Loading.close();
+                //微课刷新列表方法
+                // DeviceEventEmitter.emit("courseHeaderRefresh")
+                // DeviceEventEmitter.emit("HomeRefresh")
+
+
+                // setTimeout(
+                //     function () {
+                //         const resetAction = NavigationActions.reset({
+                //             index: 0,
+                //             actions: [NavigationActions.navigate({routeName: "Tab"})]
+                //         });
+                //         // initLicence();
+                //         // this.props.navigation.pop() && this.props.navigation.pop();
+                //         this.props.navigation.dispatch(resetAction);
+                //     }.bind(this),
+                //     1000
+                // );
+            } else {
+                alert("支付失败");
+                this.Loading.close();
+                // console.log("=====支付失败=====" + JSON.stringify(ret));
+                this.refs.toast.show("支付失败,请重新支付");
+                // await AsyncStorage.setItem("wx_data", JSON.stringify(data));
+            }
+        } catch (error) {
+            this.Loading.close();
+            console.log("------error payment-------" + error);
+        }
+    }
+
+    async tenPay() {
+        // debugger
+        const orderNo = this.state.OrderNo;
+        let tokens = await storage.get("userTokens");
+        const url = api.base_uri + "/v1/app/pay/wxGetPreyId?ordNo=" + orderNo + "&business=sport";
+        // debugger;
+        let responseData = await fetch(url, {
+            method: "get",
+            headers: {
+                "Content-Type": "application/json",
+                token: tokens.token
+            }
+        })
+            .then(resp => resp.json())
+            .then(result => {
+                // debugger
+                return new Promise((resolve, reject) => {
+                    // debugger;
+                    if (result && result.result) {
+                        //storage.save('containsStructCombo', id, result.page);
+
+                        resolve && resolve(result.result);
+                    } else {
+                        reject && reject(new Error("data parse error"));
+                    }
+                });
+            });
+        // debugger
+        return responseData;
+    }
+
+    // getOrderId = async () => {
+    //     //如果没有订单号就获取订单号  (继续支付会有订单号 点击了一次立即支付也会有订单号)
+    //     if (
+    //         this.state.OrderNo == null ||
+    //         this.state.OrderNo == "" ||
+    //         this.state.OrderNo == undefined
+    //     ) {
+    //         // alert(`================\n 进入getOrderId`);
+    //         let tokens = await storage.get("userTokens");
+    //         // alert(`================\n tokens is ${JSON.stringify(tokens)}`);
+    //         const url = api.base_uri + "/v1/app/order/addOrder";
+    //         // alert(`================\n url is ${url}`);
+    //         // debugger;
+    //         let responseData = await fetch(url, {
+    //             method: "post",
+    //             headers: {
+    //                 "Content-Type": "application/json",
+    //                 token: tokens.token
+    //             },
+    //             body: JSON.stringify({
+    //                 comboId: this.state.comboId,  //FIXME
+    //                 ordRes: "android",
+    //                 lang: "ch",
+    //                 remark: 'guke',
+    //                 priceId: this.state.priceId   //FIXME
+    //             })
+    //         })
+    //             .then(resp => resp.json())
+    //             .then(result => {
+    //                 return new Promise((resolve, reject) => {
+    //                     // alert(`result is ${JSON.stringify(result)}`)
+    //                     // debugger;
+    //                     if (result && result.order) {
+    //                         // alert(`=====\n生成的订单号为:${JSON.stringify(result)}`);
+    //                         //storage.save('containsStructCombo', id, result.page);
+
+    //                         resolve && resolve(result.order);
+    //                     } else {
+    //                         reject && reject(new Error("data parse error"));
+    //                     }
+    //                 });
+    //             });
+    //         this.setState({
+    //             OrderNo: responseData.ordNo
+    //         });
+    //     }
+    // };
     changeSelect(index) {
         console.log(index)
         this.setState({
@@ -86,10 +248,12 @@ export default class BuyVip extends BaseComponent {
         })
     }
     gotoPay() {
+        Wxpay.registerApp(api.APPID);
+                                // this.payment()
         // this.props.navigation.navigate("PaymentOrder", {
         //     combo: this.state.packageDetail[this.state.packageSelected]
         // });
-        this.newAddOrder(this.state.packageDetail[this.state.packageSelected].priceId,this.state.packageDetail[this.state.packageSelected].comboId)
+        this.payment(this.state.packageDetail[this.state.packageSelected].priceId,this.state.packageDetail[this.state.packageSelected].comboId)
     }
 
     renderPackageDetail() {
@@ -185,6 +349,14 @@ export default class BuyVip extends BaseComponent {
                 <Loading ref={r => {
                     this.Loading = r
                 }} hide={true} />
+                <Toast
+                    ref="toast"
+                    position="top"
+                    positionValue={200}
+                    fadeInDuration={750}
+                    fadeOutDuration={1000}
+                    opacity={0.8}
+                />
             </ContainerView>
         )
     }
